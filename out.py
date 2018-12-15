4c34df97147cf4c0151c397a1f654003b978c174
MatrekP <44618711+MatrekP@users.noreply.github.com>
2018-12-15 21:59:34 +0100

    Create out.py

---
+++ out.py
@@ -0,0 +1,53 @@
+{
+  "type": "FeatureCollection",
+  "generator": "overpass-ide",
+  "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+  "timestamp": "2018-12-12T19:58:03Z",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/274615055",
+        "addr:housenumber": "226/18",
+        "addr:place": "Vnitřní Město",
+        "addr:postcode": "30100",
+        "addr:street": "Sedláčkova",
+        "amenity": "bar",
+        "name": "Jazz rock café",
+        "ref:ruian": "24572853",
+        "source": "cuzk:ruian",
+        "website": "http://www.jazzrockcafe.cz",
+        "wifi": "free",
+        "cluster": 1
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          13.3755314,
+          49.7475623
+        ]
+      },
+      "id": "node/274615055"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/296636965",
+        "addr:conscriptionnumber": "109",
+        "addr:housenumber": "109/21",
+        "addr:postcode": "30100",
+        "addr:street": "Sedláčkova",
+        "addr:streetnumber": "21",
+        "source:addr": "uir_adr",
+        "uir_adr:ADRESA_KOD": "24571539",
+        "cluster": 2
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          13.3752752,
+          49.7475609
+        ]
+      },
+      "id": "node/296636965"
+    },
