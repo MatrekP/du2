5bb326b032184484a6537ddf3797fc19b1958777
MatrekP <44618711+MatrekP@users.noreply.github.com>
2019-01-03 21:47:17 +0100

    Update du2.py

--- du2.py
+++ du2.py
@@ -47,20 +47,29 @@ if len(features) <= 50:
 else:
     print("> 50")
     middle = [(bbox[0] + bbox[2]) / 2, (bbox[1] + bbox[3]) / 2]
-#     quadrant_1 = [middle[0], middle[1], bbox[2], bbox[3]]
-#     quadrant_2 = [bbox[0], middle[1], middle[0], bbox[3]]
-#     quadrant_3 = [bbox[0], bbox[1], middle[0], middle[1]]
-#     quadrant_4 = [middle[0], bbox[1], bbox[2], middle[1]]
+    # quadrant_1 = [middle[0], middle[1], bbox[2], bbox[3]]
+    # quadrant_2 = [bbox[0], middle[1], middle[0], bbox[3]]
+    # quadrant_3 = [bbox[0], bbox[1], middle[0], middle[1]]
+    # quadrant_4 = [middle[0], bbox[1], bbox[2], middle[1]]
+    features1 = []
+    features2 = []
+    features3 = []
+    features4 = []
     for feature in features:
         coordinates = feature['geometry']['coordinates']
         if coordinates[0] > middle[0] and coordinates[1] > middle[1]:
             feature['properties']['cluster_id'] = 1
+            features1.append(feature)
         if coordinates[0] <= middle[0] and coordinates[1] > middle[1]:
             feature['properties']['cluster_id'] = 2
+            features2.append(feature)
         if coordinates[0] <= middle[0] and coordinates[1] <= middle[1]:
             feature['properties']['cluster_id'] = 3
+            features3.append(feature)
         if coordinates[0] > middle[0] and coordinates[1] <= middle[1]:
             feature['properties']['cluster_id'] = 4
+            features4.append(feature)
+    # print(features1)
 
 
 # print(json.dumps(json_map, ensure_ascii=False, indent=2))
