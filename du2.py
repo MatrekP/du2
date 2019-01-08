77ff00a429d13ac08ca546430c9e96a64dae2ef9
MatrekP <44618711+MatrekP@users.noreply.github.com>
2019-01-08 23:46:57 +0100

    Update du2.py

--- du2.py
+++ du2.py
@@ -21,6 +21,40 @@ def calculate_bbox(features):
     return minx, miny, maxx, maxy
 
 
+def quadtree(features, cluster):
+    if len(features) <= 50:
+        print("<= 50")
+        # cluster += 1
+        for feature in features:
+            feature['properties']['cluster_id'] = cluster
+    else:
+        print("> 50")
+        middle = [(bbox[0] + bbox[2]) / 2, (bbox[1] + bbox[3]) / 2]
+        features1 = []
+        features2 = []
+        features3 = []
+        features4 = []
+        for feature in features:
+            coordinates = feature['geometry']['coordinates']
+            if coordinates[0] > middle[0] and coordinates[1] > middle[1]:
+                # feature['properties']['cluster_id'] = cluster + 1
+                features1.append(feature)
+            if coordinates[0] <= middle[0] and coordinates[1] > middle[1]:
+                # feature['properties']['cluster_id'] = cluster + 2
+                features2.append(feature)
+            if coordinates[0] <= middle[0] and coordinates[1] <= middle[1]:
+                # feature['properties']['cluster_id'] = cluster + 3
+                features3.append(feature)
+            if coordinates[0] > middle[0] and coordinates[1] <= middle[1]:
+                # feature['properties']['cluster_id'] = cluster + 4
+                features4.append(feature)
+        quadtree(features1, cluster + 0)
+        quadtree(features2, cluster + 1)
+        quadtree(features3, cluster + 2)
+        quadtree(features4, cluster + 3)
+        # print(features1)
+
+
 # nacte vstupni geojson
 with open ('input.geojson', 'r', encoding='utf-8') as f:
     json_map = json.loads(f.read())
@@ -38,36 +72,9 @@ bbox = list(calculate_bbox(features))
 print(bbox)
 
 
-cluster = 0
 print(len(features))
-if len(features) <= 50:
-    print("<= 50")
-    cluster += 1
-    for feature in features:
-        feature['properties']['cluster_id'] = cluster
-else:
-    print("> 50")
-    middle = [(bbox[0] + bbox[2]) / 2, (bbox[1] + bbox[3]) / 2]
-    features1 = []
-    features2 = []
-    features3 = []
-    features4 = []
-    for feature in features:
-        coordinates = feature['geometry']['coordinates']
-        if coordinates[0] > middle[0] and coordinates[1] > middle[1]:
-            feature['properties']['cluster_id'] = cluster + 1
-            features1.append(feature)
-        if coordinates[0] <= middle[0] and coordinates[1] > middle[1]:
-            feature['properties']['cluster_id'] = cluster + 2
-            features2.append(feature)
-        if coordinates[0] <= middle[0] and coordinates[1] <= middle[1]:
-            feature['properties']['cluster_id'] = cluster + 3
-            features3.append(feature)
-        if coordinates[0] > middle[0] and coordinates[1] <= middle[1]:
-            feature['properties']['cluster_id'] = cluster + 4
-            features4.append(feature)
-    cluster += 4
-    # print(features1)
+cluster = 1
+quadtree(features, cluster)
 
 
 # print(json.dumps(json_map, ensure_ascii=False, indent=2))
